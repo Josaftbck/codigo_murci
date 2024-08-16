a1bf0830dd4b5c1b7411e17d6729977e61989f24
Josaftbck <josaftbck@gmail.com>
2024-08-16 11:54:46 -0600

    first version 1.0 whith Layout

---
+++ src/vista/main.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vista;
+
+/**
+ *
+ * @author Josafat bk
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+   
+        // TODO code application logic here
+        public static void main(String[] args) {
+        // TODO code application logic here
+        frm_principal fm = new frm_principal();
+        fm.show();
+    }
+    }
+    
+
